8d8346c26ec71f29eaa9ab805958c9b582d58b60
Huntsmitty <143479798+Huntsmitty@users.noreply.github.com>
2024-02-17 01:22:54 -0700

    Add files via upload

--- manager.py
+++ manager.py
@@ -2,13 +2,7 @@
 import socket
 import random
 
-class Peer:
-    def __init__(self, peername, ipv4addr, mport, pport, status):
-        self.peername = peername
-        self.ipv4addr = ipv4addr
-        self.mp = mport
-        self.pp = pport
-        self.status = status
+from peer import Peer
 
 class DHT:
     def __init__(self, peername, n, year):
